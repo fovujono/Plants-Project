0d14bab98ab4546e5ce94814c72be12126a8b45b
smoot <smoothtacomann@gmail.com>
2019-02-03 14:46:11 -0500

    modal now has sign in form and tree picture

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import React from "react";
+import "./style.css";
+import { Button, Image, Modal } from "semantic-ui-react";
 
-const ModalModalExample = () => (
-  <Modal trigger={<Button>Sign In</Button>}>
-    <Modal.Header>Select a Photo</Modal.Header>
+ class ModalModalExample extends React.Component{
+
+    render(){
+        return <Modal trigger={<Button>Sign In</Button>}>
+    <Modal.Header>Sign in Page</Modal.Header>
     <Modal.Content image>
-      <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+      <Image
+        wrapped
+        size="large"
+        src="https://images.pexels.com/photos/1313807/pexels-photo-1313807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=1200"
+      />
       <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We've found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
+        <div className="login-page">
+          <div className="form">
+            <form className="register-form">
+              <input type="text" placeholder="name" />
+              <input type="password" placeholder="password" />
+              <input type="text" placeholder="email address" />
+              <button>create</button>
+              <p className="message">
+                Already registered? <a href="/#">Sign In</a>
+              </p>
+            </form>
+            <form className="login-form">
+              <input type="text" placeholder="email" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+              <p className="message">
+                Not registered? <a href="/signup">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </Modal.Description>
     </Modal.Content>
   </Modal>
-)
+         }
+    }
 
-export default ModalModalExample
+
+
+export default ModalModalExample;
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -3,6 +3,7 @@ import React from "react";
 const Signup =() =>{
     return(
         <div className="signup-container">
+        sign up heresdsds 
 
         </div>
     )
