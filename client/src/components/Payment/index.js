48bd0b4365fdd24f89015910517ac9ff248b2a3e
smoot <smoothtacomann@gmail.com>
2019-02-08 14:33:40 -0500

    can now type in input box without react giving an error

--- client/src/components/Payment/index.js
+++ client/src/components/Payment/index.js
@@ -1,56 +1,54 @@
 import React from "react";
-import './style.css'
-import '../../pages/products';
+import "./style.css";
+import "../../pages/products";
 class Payment extends React.Component {
   constructor(props) {
     super(props);
 
- 
     this.state = {
       count: 0
     };
   }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count +1 });
+    this.setState({ count: this.state.count + 1 });
   };
 
   handleDecrement = () => {
-    if(this.state.count <= 0){
-      this.setState(prevState => ({counter: prevState.count -1}))
+    if (this.state.count <= 0) {
+      this.setState(prevState => ({ counter: prevState.count - 1 }));
+    } else {
+      this.setState({ count: this.state.count - 1 });
     }
-    else{
-      this.setState({ count: this.state.count-1})
-    }
-  }
+  };
 
-  // addtToCart = () => {
-    
-  // }
+  onInputChange = value => {
+    this.setState({ count: value });
+  };
 
   render() {
     return (
       <div>
-      <div className="payment-container">
-   
+        <div className="payment-container">
+          <button className="decrement" onClick={this.handleDecrement}>
+            -
+          </button>
 
-    
-          <button className="decrement" onClick={this.handleDecrement}>-</button>
-      
- 
-    <input type="text" value={this.state.count}></input>
- 
-     
-          <button className="increment" onClick={this.handleIncrement}>+</button>
-      
+          <input
+            type="text"
+            value={this.state.count}
+            onChange={event => this.onInputChange(event.target.value)}
+          />
 
-   
+          <button className="increment" onClick={this.handleIncrement}>
+            +
+          </button>
+        </div>
+
+        <button className="add-to-cart">Add To Cart</button>
       </div>
-      
-      <button className="add-to-cart">Add To Cart</button>
-    </div>
     );
-  } 
+  }
 }
 
 export default Payment;
