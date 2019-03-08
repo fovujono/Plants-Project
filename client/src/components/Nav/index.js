6953e62c73041cbf83b9b8bf08f0672d54c90c24
smoot <smoothtacomann@gmail.com>
2019-03-08 18:41:44 -0500

    new logo

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,7 +8,7 @@ const Nav = (props) => {
   <div className="navbar-content">
     <div className="ui inverted segment" id="navbar-background">
       <div className="ui inverted secondary menu">
-      <div className="logo"><a href="/" className="logo">Toledo's Plants</a></div>
+      <div className="logo"><a href="/" className="logo">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="font-one">TOLEDO'S</span> &nbsp;<span class="font-two">PLANTS</span></a></div>
          {props.children}
         <a className="item right floated" id="about-us"href="/about"> 
             About Us
--- server.js
+++ server.js
@@ -16,11 +16,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plants");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plants");
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/plants";
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 // Start the API server
