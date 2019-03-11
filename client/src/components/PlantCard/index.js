f7fecc1f995c71f64c9e819c4c0f33c07887ea05
smoot <smoothtacomann@gmail.com>
2019-03-11 16:34:25 -0400

    override library styling

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import Payment from "../Payment";
-import Flippy, {FrontSide, BackSide} from 'react-flippy';
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
 class PlantCard extends React.Component {
   constructor() {
@@ -15,51 +15,54 @@ class PlantCard extends React.Component {
     this.setState({
       isFlipped: !this.state.isFlipped
     });
-  }
+  };
 
   render() {
     return (
       <div className="plant-container">
-       <Flippy
-    flipOnHover={false} // default false
-    flipOnClick={false} // default false
-    flipDirection="horizontal" // horizontal or vertical
-    ref={(r) => this.flippyHorizontal = r} // to use toggle method like this.flippy.toggle()
-    // if you pass isFlipped prop component will be controlled component.
-    // and other props, which will go to div
-   
-  >
-    <FrontSide>
-        <div className="card">
-      <div className="img-container">
-        <img alt={this.props.plantName} src={this.props.image} />
-      </div>
-      <div className="content">
-        <p> {this.props.plantName}</p>
-        <p>${this.props.price}</p>
-        {/* <p>{props.stock}</p>  */}
-        <Payment
-          key={this.props._id}
-          id={this.props._id}
-          image={this.props.image}
-          plantName={this.props.plantName}
-          price={this.props.price}
-          stock={this.props.stock}
-        />
-                  
-         
-              <button type="button" onClick={() => this.flippyHorizontal.toggle()}>Toggle Me!</button>
-         
-      </div>
-    </div>
-    </FrontSide>
-    <BackSide style={{ backgroundColor: '#175852'}}>
-      {this.props.price}
+        <Flippy
+        
+          flipOnHover={false} // default false
+          flipOnClick={false} // default false
+          flipDirection="horizontal" // horizontal or vertical
+          ref={r => (this.flippyHorizontal = r)} // to use toggle method like this.flippy.toggle()
+          // if you pass isFlipped prop component will be controlled component.
+          // and other props, which will go to div
+        >
+          <FrontSide style={{padding:"0rem", boxShadow:"none"}} >
+            <div className="card">
+            <a type="button" onClick={() => this.flippyHorizontal.toggle()}>
+                  Learn about plant
+                </a>
+              <div className="img-container">
+                <img alt={this.props.plantName} src={this.props.image} />
+              </div>
+              <div className="content">
+                <p> {this.props.plantName}</p>
+                <p>${this.props.price}</p>
+               
+                <Payment
+                  key={this.props._id}
+                  id={this.props._id}
+                  image={this.props.image}
+                  plantName={this.props.plantName}
+                  price={this.props.price}
+                  stock={this.props.stock}
+                />
 
-      <button type="button" onClick={() => this.flippyHorizontal.toggle()}>Toggle Me!</button>
-    </BackSide>
-  </Flippy>
- 
+            
+              </div>
+            </div>
+          </FrontSide>
+          <BackSide style={{ backgroundColor: "#175852" }}>
+          <a type="button" onClick={() => this.flippyHorizontal.toggle()}>
+              Go Back
+            </a>
+            {this.props.price}
+
+            
+          </BackSide>
+        </Flippy>
       </div>
     );
   }
