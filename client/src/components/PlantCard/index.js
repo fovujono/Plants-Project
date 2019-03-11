797dd68ddfabc742482b721ea356af178deb1468
smoot <smoothtacomann@gmail.com>
2019-03-11 14:35:50 -0400

    added flippy-react npm package to flip cards

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -1,28 +1,53 @@
 import React from "react";
 import "./style.css";
 import Payment from "../Payment";
+import Flippy, {FrontSide, BackSide} from 'react-flippy';
 
-const PlantCard = props => {
-  return (
-    <div className="card">
+class PlantCard extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="plant-container">
+       <Flippy
+    flipOnHover={false} // default false
+    flipOnClick={true} // default false
+    flipDirection="horizontal" // horizontal or vertical
+    ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+    // if you pass isFlipped prop component will be controlled component.
+    // and other props, which will go to div
+   
+  >
+    <FrontSide>
+        <div className="card">
       <div className="img-container">
-        <img alt={props.plantName} src={props.image} />
+        <img alt={this.props.plantName} src={this.props.image} />
       </div>
       <div className="content">
-        <p> {props.plantName}</p>
-        <p>${props.price}</p>
+        <p> {this.props.plantName}</p>
+        <p>${this.props.price}</p>
         {/* <p>{props.stock}</p>  */}
         <Payment
-          key={props._id}
-          id={props._id}
-          image={props.image}
-          plantName={props.plantName}
-          price={props.price}
-          stock={props.stock}
+          key={this.props._id}
+          id={this.props._id}
+          image={this.props.image}
+          plantName={this.props.plantName}
+          price={this.props.price}
+          stock={this.props.stock}
         />
       </div>
     </div>
-  );
-};
+    </FrontSide>
+    <BackSide>
+      ROCKS
+    </BackSide>
+  </Flippy>
+ 
+      </div>
+    );
+  }
+}
 
 export default PlantCard;
