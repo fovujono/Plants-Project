6d7fdfb724b4e6149f01883348033d4bb40a51d7
smoot <smoothtacomann@gmail.com>
2019-04-14 15:15:35 -0400

    changed the url for request

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3004;
 
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
